d5a4b21dbd3fa2fc59cda65f130d7f6b7e40b055
Elpera <60768248+Elpera@users.noreply.github.com>
2022-11-17 16:38:46 +0100

    Update EmployeesList.js

--- frontendv2/src/components/EmployeesList.js
+++ frontendv2/src/components/EmployeesList.js
@@ -139,27 +139,3 @@ const EmployeesList = (props) => {
 };
 
 export default EmployeesList;
-
-
-/*
-<div className="col-md-8">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search by email"
-            defaultValue={searchEmail}
-            onChange={onChangeSearchTitle}
-          />
-          <div className="input-group-append">
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              onClick={findByEmail}
-            >
-              Search
-            </button>
-          </div>
-        </div>
-      </div>
-*/
