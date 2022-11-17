2592b3487427dd53a0873bb92537254383c22b12
Sameer Abdullah Bin Azad <Sameer.Abdullah@solera.com>
2022-11-17 11:41:33 +0100

    Frontend v0.1

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+import registrationF
+
+export default function App() {
+    return( 
+            <div className="App">
+              <RegistrationForm/>
+            </div>
+    );
+}
